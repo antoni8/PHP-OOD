a6b637b83500191cdbb5ef3e6356f2430f546795
Antoni <inotna.21@gmail.com>
2021-01-14 20:42:59 +0100

    exceptions_1

---
+++ exceptions_1.php
@@ -0,0 +1,32 @@
+<?php
+// sense excepcions
+/*function discountCalculation($discount,$percentage,$price){
+  //calculate the final amount after applying the discount and return it
+  //considering $discount as a %
+  if($percentage!=0){
+  $discountCalculation=($price * $discount)/$percentage;
+    return $price - $discountCalculation;
+  }
+  return "value has to be greater than 0";
+}
+
+echo discountCalculation(10,0,50);*/
+
+//amb excepcions
+function discountCalculation($discount,$percentage,$price){
+  //calculate the final amount after applying the discount and return it
+  //considering $discount as a %
+  if($percentage==0)
+    throw new Exception("Divisió per 0.");
+  else {
+      $discountCalculation=($price * $discount)/$percentage;
+      return $price - $discountCalculation;
+  }
+}
+try {
+  echo discountCalculation(10,100,50);
+}
+catch (Exception $e) {
+    echo "S'ha capturat l'excepció: " . $e->getMessage() . "<br>";
+}
+?>
